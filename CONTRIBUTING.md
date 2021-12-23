a41fcca852e42ab702a1c217ef417381b65886c9
Anton-Augustsson <Anton.Augustsson.4804@student.uu.se>
2021-12-23 09:11:40 +0100

    added contributing and extension file. Also made a class for inputs

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+# Resources 
+- [GnomeShell Extensions](https://wiki.gnome.org/Projects/GnomeShell/Extensions)
+- [Developers](https://gjs.guide/extensions/development/creating.html#gnome-extensions-tool)
+- [Gnome Extensions example](https://gitlab.com/justperfection.channel/how-to-create-a-gnome-shell-extension/-/blob/master/example@example.com/extension.js)
+- [Youtube tutorial](https://www.youtube.com/watch?v=iMyR5lJf7dU)
+
+The genom shell extensions are eather lokated in:
+```
+ /usr/share/gnome-shell/extensions
+```
+or
+```
+ ~/.local/share/gnome-shell/extensions
+```
--- src/tts.py
+++ src/tts.py
@@ -3,40 +3,16 @@ from gtts import gTTS
 import sys, getopt
 import os 
 
-def main(argv=sys.argv[1:]):
-   """Text to speech
-   
-   Example:
-      # read text
-      tts.py --read="read this text"  
+class Inputs:
+   def __init__(self, args):
+      self.speed       = ''
+      self.input_file  = '' 
+      self.output_file = ''
+      self.input_text  = ''
+      self.speak       = False
+      self.readInput(argv=args)
 
-      # read text with speed x2
-      tts.py --read="read this text" --speed="2"
-
-      # read txt file and save to mp3:   
-      tts.py -i someInputFile.txt -o someOutputFile.mp3  
-   """
-
-   inputs = {
-      "input_file": '',
-      "output_file": '',
-      "input_text": '',
-      "speed": 1,
-      "speak": False 
-   }
-
-   def saveMp3(text="", output_file="output_file.mp3"):
-      """Save the text to read to the output_file"""
-      tts = gTTS(text)
-      print(output_file)
-      tts.save(output_file)
-    
-   def speak(output_file="output_file.mp3", speed=1):
-      """Read out load the imputed text"""
-      os.system(f'mpg123 --doublespeed "{speed}" ' + output_file)
-      sys.exit(0)
-
-   def readInput(argv=argv, inputs=inputs):
+   def readInput(self, argv):
       """Interpreted the inputs and save it into dictionary (inputs)"""
 
       # The available options to have as arguments when runing the program
@@ -58,26 +34,22 @@ def main(argv=sys.argv[1:]):
          print('test.py -i <inputfile> -o <outputfile>')
          sys.exit()
 
-      def opt_read(inputs, arg):
+      def opt_read(arg):
          """Selects the speak option and saves the text to speak"""
-         inputs["input_text"] = arg
-         inputs["speak"] = True
-         return inputs
+         self.input_text = arg
+         self.speak      = True
 
-      def opt_speed(inputs, arg):
+      def opt_speed(arg):
          """Changes the speed of the speaker"""
-         inputs["speed"] = int(arg)
-         return inputs
+         self.speed = int(arg)
 
-      def opt_input_file(inputs, arg):
+      def opt_input_file(arg):
          """Sets the input file"""
-         inputs["input_file"] = arg
-         return inputs
+         self.input_file = arg
 
-      def opt_output_file(inputs, arg):
+      def opt_output_file(arg):
          """Sets the output file"""
-         inputs["output_file"] = arg
-         return inputs
+         self.output_file = arg
 
       # Try to read the inputs
       try:
@@ -87,35 +59,59 @@ def main(argv=sys.argv[1:]):
          opt_error()
       # If the inputs could be obtained interpret them
       for opt, arg in opts:
-         if opt == options["help"]: 
-            opt_help()
-         elif opt in options["read"]:        
-            inputs = opt_read(inputs, arg) 
-         elif opt in options["speed"]:        
-            inputs = opt_speed(inputs, arg) 
-         elif opt in options["input"]: 
-            inputs = opt_input_file(inputs, arg)
-         elif opt in options["output"]: 
-            inputs = opt_output_file(inputs, arg)
+         if opt == options["help"]:     opt_help()
+         elif opt in options["read"]:   opt_read(arg) 
+         elif opt in options["speed"]:  opt_speed(arg) 
+         elif opt in options["input"]:  opt_input_file(arg)
+         elif opt in options["output"]: opt_output_file(arg)
 
-      return inputs
+   def get_input_text(self):  return self.input_text
+   def get_input_file(self):  return self.input_file
+   def get_output_file(self): return self.output_file
+   def get_speed(self):       return self.speed
+   def get_speak(self):       return self.speak
+
+
+def main(argv=sys.argv[1:]):
+   """Text to speech
+   
+   Example:
+      # read text
+      tts.py --read="read this text"  
+
+      # read text with speed x2
+      tts.py --read="read this text" --speed="2"
+
+      # read txt file and save to mp3:   
+      tts.py -i someInputFile.txt -o someOutputFile.mp3  
+   """
+
+   def saveMp3(text="", output_file="output_file.mp3"):
+      """Save the text to read to the output_file"""
+      tts = gTTS(text)
+      print(output_file)
+      tts.save(output_file)
+    
+   def speak(output_file="output_file.mp3", speed=1):
+      """Read out load the imputed text"""
+      os.system(f'mpg123 --doublespeed "{speed}" ' + output_file)
+      sys.exit(0)
 
    # First Read the input
-   inputs = readInput()
+   inputs = Inputs(args=argv)
 
    # Save input text to file
-   if inputs["input_file"]:
-        f = open(inputs["input_file"],"r")
+   if inputs.get_input_file():
+        f = open(inputs.get_input_file(),"r")
         inputs.text = f.read()
-        saveMp3(text=inputs["input_text"], output_file=inputs["output_file"])
+        saveMp3(text=inputs.get_input_text(), output_file=inputs.get_output_file())
         f.close()
 
    # Read input text out loud
-   if inputs["speak"] and inputs["input_text"]:
-        saveMp3(text=inputs["input_text"], output_file="output_file.mp3")
-        speak(speed=inputs["speed"], output_file="output_file.mp3")
+   if inputs.get_speak() and inputs.get_input_text():
+        saveMp3(text=inputs.get_input_text(), output_file="output_file.mp3")
+        speak(speed=inputs.get_speed(), output_file="output_file.mp3")
 
 
 if __name__ == "__main__":
    main(sys.argv[1:])
-
