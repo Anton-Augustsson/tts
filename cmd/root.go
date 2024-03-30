f86a23856acabc519d4bf6e5c3315741801cbcf5
Anton Augustsson <anton.augustsson99@gmail.com>
2024-03-30 21:52:13 +0100

    Started Optimizing

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,21 @@
 package cmd
 
 import (
-	"os"
-	"log"
-	"fmt"
 	"bytes"
+	"fmt"
+	"log"
+	"os"
 	"time"
+
 	"github.com/spf13/cobra"
+
 	//"golang.design/x/clipboard"
 	"github.com/atotto/clipboard"
 	//"github.com/Duckduckgot/gtts"
 
 	//htgotts "github.com/hegedustibor/htgo-tts"
-  //handlers "github.com/hegedustibor/htgo-tts/handlers"
-  //voices "github.com/hegedustibor/htgo-tts/voices"
+	//handlers "github.com/hegedustibor/htgo-tts/handlers"
+	//voices "github.com/hegedustibor/htgo-tts/voices"
 
 	"github.com/amitybell/piper" // Import the piper package for text-to-speech
 	// jenny "github.com/amitybell/piper-voice-jenny" // Jenny's voice is available but not used in this example
@@ -24,62 +26,77 @@ import (
 	"github.com/gopxl/beep/wav"     // Import the wav package to decode the wav audio
 )
 
+func prepare(tts *piper.TTS, content string) (*beep.StreamSeekCloser, *beep.Format, error) {
+	wavBytes, err := tts.Synthesize(content)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	r := bytes.NewReader(wavBytes)
+
+	streamer, format, err := wav.Decode(r)
+	if err != nil {
+		return &streamer, &format, err
+	}
+
+	return &streamer, &format, nil
+}
+
+func play(streamer *beep.StreamSeekCloser, done chan bool) {
+	speaker.Play(beep.Seq(*streamer, beep.Callback(func() {
+		done <- true
+	})))
+}
+
+func read(content string) {
+	tts, err := piper.New("", alan.Asset)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	midpoint := len(content) / 2
+	firstHalf := content[:midpoint]
+	secondHalf := content[midpoint:]
+
+	streamer, format, err := prepare(tts, firstHalf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan bool)
+	play(streamer, done)
+	streamer, format, err = prepare(tts, secondHalf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done = make(chan bool)
+	play(streamer, done)
+	<-done
+}
+
 var rootCmd = &cobra.Command{
-  Use:   "hugo",
-  Short: "Hugo is a very fast static site generator",
-  Long: `A Fast and Flexible Static Site Generator built with
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
                 love by spf13 and friends in Go.
                 Complete documentation is available at http://hugo.spf13.com`,
-  Run: func(cmd *cobra.Command, args []string) {
-    log.Println("hugo func")
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("hugo func")
 		content, err := clipboard.ReadAll()
 		if err != nil {
 			log.Println("Error reading clipboard:", err)
 			return
 		}
 		log.Println(content)
-		//speech := htgotts.Speech{Folder: "audio", Language: voices.English, Handler: &handlers.MPlayer{}}
-		//speech.Speak(content)
-			// Initialize a new piper instance with Alan's voice
-		tts, err := piper.New("", alan.Asset)
-		if err != nil {
-			panic(err) // If there's an error, stop the program
-		}
-
-		// Use the piper instance to synthesize speech from text
-		wavBytes, err := tts.Synthesize(content)
-		if err != nil {
-			panic(err) // If there's an error, stop the program
-		}
-
-		// Create an io.Reader from the byte array
-		r := bytes.NewReader(wavBytes)
-
-		// Decode the WAV data into a format that beep can understand
-		streamer, format, err := wav.Decode(r)
-		if err != nil {
-			panic(err) // If there's an error, stop the program
-		}
-
-		// Initialize the speaker with the sample rate from the WAV data
-		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-		// Create a done channel to signal when the audio has finished playing
-		done := make(chan bool)
-
-		// Play the audio and signal the done channel when finished
-		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-			done <- true // Signal the done channel
-		})))
-
-		// Wait for the audio to finish playing before allowing the program to exit
-		<-done
-  },
+		read(content)
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
