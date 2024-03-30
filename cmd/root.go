7c7d69cf59d96570c3c3768e17556018303a17e0
Anton Augustsson <anton.augustsson99@gmail.com>
2024-03-30 19:29:00 +0100

    tts working

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,24 @@ import (
 	"os"
 	"log"
 	"fmt"
+	"bytes"
+	"time"
 	"github.com/spf13/cobra"
+	//"golang.design/x/clipboard"
+	"github.com/atotto/clipboard"
+	//"github.com/Duckduckgot/gtts"
+
+	//htgotts "github.com/hegedustibor/htgo-tts"
+  //handlers "github.com/hegedustibor/htgo-tts/handlers"
+  //voices "github.com/hegedustibor/htgo-tts/voices"
+
+	"github.com/amitybell/piper" // Import the piper package for text-to-speech
+	// jenny "github.com/amitybell/piper-voice-jenny" // Jenny's voice is available but not used in this example
+	alan "github.com/amitybell/piper-voice-alan" // Import Alan's voice from the piper package
+
+	"github.com/gopxl/beep"         // Import the beep package for playing audio
+	"github.com/gopxl/beep/speaker" // Import the speaker package to output the audio
+	"github.com/gopxl/beep/wav"     // Import the wav package to decode the wav audio
 )
 
 var rootCmd = &cobra.Command{
@@ -15,6 +32,48 @@ var rootCmd = &cobra.Command{
                 Complete documentation is available at http://hugo.spf13.com`,
   Run: func(cmd *cobra.Command, args []string) {
     log.Println("hugo func")
+		content, err := clipboard.ReadAll()
+		if err != nil {
+			log.Println("Error reading clipboard:", err)
+			return
+		}
+		log.Println(content)
+		//speech := htgotts.Speech{Folder: "audio", Language: voices.English, Handler: &handlers.MPlayer{}}
+		//speech.Speak(content)
+			// Initialize a new piper instance with Alan's voice
+		tts, err := piper.New("", alan.Asset)
+		if err != nil {
+			panic(err) // If there's an error, stop the program
+		}
+
+		// Use the piper instance to synthesize speech from text
+		wavBytes, err := tts.Synthesize(content)
+		if err != nil {
+			panic(err) // If there's an error, stop the program
+		}
+
+		// Create an io.Reader from the byte array
+		r := bytes.NewReader(wavBytes)
+
+		// Decode the WAV data into a format that beep can understand
+		streamer, format, err := wav.Decode(r)
+		if err != nil {
+			panic(err) // If there's an error, stop the program
+		}
+
+		// Initialize the speaker with the sample rate from the WAV data
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+		// Create a done channel to signal when the audio has finished playing
+		done := make(chan bool)
+
+		// Play the audio and signal the done channel when finished
+		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+			done <- true // Signal the done channel
+		})))
+
+		// Wait for the audio to finish playing before allowing the program to exit
+		<-done
   },
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,23 @@ module github.com/Anton-Augustsson/tts
 go 1.20
 
 require (
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/amitybell/piper v0.0.0-20240130090909-b170e42b1e09 // indirect
+	github.com/amitybell/piper-asset v0.0.0-20231030194325-d36a29e3b1fd // indirect
+	github.com/amitybell/piper-bin-linux v0.0.0-20231118093037-92b3de178ad8 // indirect
+	github.com/amitybell/piper-bin-windows v0.0.0-20231118093113-cc2cef2f6b74 // indirect
+	github.com/amitybell/piper-voice-alan v0.0.0-20231118093148-059963c24dbd // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/ebitengine/oto/v3 v3.1.0 // indirect
+	github.com/ebitengine/purego v0.5.0 // indirect
+	github.com/gopxl/beep v1.4.0 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/hajimehoshi/oto/v2 v2.3.1 // indirect
+	github.com/hegedustibor/htgo-tts v0.0.0-20230402053941-cd8d1a158135 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
